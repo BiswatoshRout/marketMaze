f089abd377a908b1c850448f1e3783014c693e48
BiswatoshRout <routlikun@gmail.com>
2025-08-26 22:10:16 +0530

    Initial Commit Main

---
+++ OrderService/src/main/java/controller/OrderServiceMain.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class OrderServiceMain {
+    public static void main(String[] args){
+        System.out.println("Hello From Order Service");
+    }
+}
---
+++ UserService/src/main/java/controller/UserServiceMain.java
@@ -0,0 +1,7 @@
+package controller;
+
+public class UserServiceMain {
+    public static void main(String[] args) {
+        System.out.println("Hello from User Service");
+    }
+}
